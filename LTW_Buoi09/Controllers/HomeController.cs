60380dcbdb68fa6e34af5b611ad481676fa64fca
tanphat12a1 <tanphat6g@gmail.com>
2025-11-06 19:08:30 +0700

    First commit - upload source code

---
+++ LTW_Buoi09/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LTW_Buoi09.Models;
+using System.Data.SqlClient;
+namespace LTW_Buoi09.Controllers
+{
+    public class HomeController : Controller
+    {
+        QLBSDataContext data = new QLBSDataContext();
+        public ActionResult Index()
+        {
+            List<Sach> dsS = data.Saches.Take(10).ToList();
+            return View(dsS);
+        }
+        public ActionResult DetailsIndex(int maS)
+        {
+            Sach s = data.Saches.FirstOrDefault(m => m.MaSach == maS);
+            List<ThamGia> dsTG = data.ThamGias.Where(mtg=>mtg.MaSach == maS).ToList();
+            ViewBag.tg = dsTG;
+            return View(s);
+        }
+        public ActionResult IndexTheoChuDE(int maCD)
+        {
+            List<ChuDe> dsCD = data.ChuDes.ToList();
+            return View(dsCD);
+        }
+        public ActionResult IndexTheoNXB(int maNXB)
+        {
+            List<NhaXuatBan> dsNXB = data.NhaXuatBans.ToList();
+            return View(dsNXB);
+        }
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Search()
+        {
+            ViewBag.MaChuDe = new SelectList(data.ChuDes.ToList(), "MaChuDe", "TenChuDe");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult XLSearch(string txtTuKhoa, int MaChuDe)
+        {
+            List<Sach> dsS = data.Saches.Where(it=>it.TenSach.Contains(txtTuKhoa) && it.MaChuDe == MaChuDe).ToList();
+            return View("Index", dsS);
+        }
+    }
+}
---
+++ LTW_Buoi09/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LTW_Buoi09.MvcApplication" Language="C#" %>
